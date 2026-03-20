1b2da8b0f3fddd29839888e5662951008240d17f
perkinslr <logan@github.lp-programming.com>
2026-03-20 15:22:16 -0700

    Update for 1.6

---
+++ Source/1.6/Shields/Utilities/Graphics.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace Jaxxa.EnhancedDevelopment.Shields.Shields.Utilities
+{
+    public static class Graphics
+    {
+        private static Mesh CircleMesh_cache;
+        public static Mesh CircleMesh
+        {
+            get
+            {
+
+                if (CircleMesh_cache != null)
+                    return CircleMesh_cache;
+                else
+                {
+                    //Random radius
+                    double radius = 1f;
+
+                    //List of points in the circle
+                    List<Vector2> list = new List<Vector2>();
+                    list.Add(new Vector2(0f, 0f));
+                    for (int i = 0; i <= 360; i += 4)
+                    {
+                        double num = i / 180f * Math.PI;
+                        //Create the point
+                        list.Add(new Vector2(0f, 0f)
+                        {
+                            x = (float)((double)radius * Math.Cos((double)num)),
+                            y = (float)((double)radius * Math.Sin((double)num))
+                        });
+                    }
+                    //Now convert the points in V3 array
+                    List<Vector3> array = new List<Vector3>(list.Count);
+                    for (int j = 0; j < list.Count; j++)
+                    {
+                        array[j] = new Vector3(list[j].x, 0f, list[j].y);
+                    }
+
+                    //Some magic
+                    Triangulator triangulator = new Triangulator(array);
+                    List<int> triangles = triangulator.Triangulate();
+                    //Create the mesh object
+                    CircleMesh_cache = new Mesh();
+                    CircleMesh_cache.vertices = array.ToArray();
+                    CircleMesh_cache.uv = new Vector2[list.Count];
+                    CircleMesh_cache.triangles = triangles.ToArray();
+                    CircleMesh_cache.RecalculateNormals();
+                    CircleMesh_cache.RecalculateBounds();
+                    return CircleMesh_cache;
+                }
+            }
+        }
+        
+    }
+}
